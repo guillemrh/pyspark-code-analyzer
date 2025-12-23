1f2a203afa3afc6cc039815a2c36d81fd5e9eba1
guillemrh <guillemmm.98@gmail.com>
2025-12-23 19:00:37 +0100

    Add Spark operation registry with transformation and action types

---
+++ backend/app/parsers/spark_ops.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+class OpType(str, Enum):
+    TRANSFORMATION = "transformation"
+    ACTION = "action"
+
+
+# Canonical Spark operation registry
+SPARK_OPS = {
+    # Transformations
+    "select": OpType.TRANSFORMATION,
+    "filter": OpType.TRANSFORMATION,
+    "withColumn": OpType.TRANSFORMATION,
+    "groupBy": OpType.TRANSFORMATION,
+    "join": OpType.TRANSFORMATION,
+    "distinct": OpType.TRANSFORMATION,
+    "drop": OpType.TRANSFORMATION,
+    "repartition": OpType.TRANSFORMATION,
+    "coalesce": OpType.TRANSFORMATION,
+
+    # Actions
+    "count": OpType.ACTION,
+    "collect": OpType.ACTION,
+    "show": OpType.ACTION,
+    "take": OpType.ACTION,
+    "foreach": OpType.ACTION,
+    "write": OpType.ACTION,
+}
